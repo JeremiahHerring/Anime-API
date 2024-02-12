5335d40405b5f69baa97e17fbe212beccaa38859
Jeremiah <jeremherring@gmail.com>
2024-02-11 22:16:10 -0800

    final version

--- anime-db/src/App.js
+++ anime-db/src/App.js
@@ -1,16 +1,35 @@
-import './Components/styles.css'
-import { useEffect, useState } from 'react';
-import { AnimeList } from "./Components/AnimeList.js"
-import { AnimeInfo } from './Components/AnimeInfo.js';
-
+import React, { useEffect, useState } from "react";
+import './Components/styles.css';
+import { AnimeList } from "./Components/AnimeList";
+import { AnimeInfo } from "./Components/AnimeInfo";
+import { AddToList } from "./Components/AddToList";
+import { RemoveFromList } from "./Components/RemoveFromList";
 function App() {
   // useState() - used to handle reactive data. Any data that changes is called state, and everytime the state changes,
   //              we need this to be reflected in the UI
   // Returns an array with two values - [Reactive Value, Setter]
   // Whenever data is referenced it will change the UI, Setter will listen to any changes in the data
-  const [search, setSearch] = useState('Naruto') // Initial state value 
-  const [animeData, setAnimeData] = useState()
-  const [animeInfo, setAnimeInfo] = useState()
+  const [search,setSearch]=useState('Naruto') // initial state value
+  const [animeData,setAnimeData]=useState();
+  const [animeInfo,setAnimeInfo]=useState()
+  const [myAnimeList,setMyAnimeList]=useState([])
+
+  const addTo=(anime)=>{
+    const index=myAnimeList.findIndex((myanime)=>{
+        return myanime.mal_id === anime.mal_id
+    })
+    if(index < 0){
+      const newArray=[...myAnimeList,anime]
+      setMyAnimeList(newArray);
+    }
+    
+  }
+  const removeFrom=(anime)=>{
+    const newArray=myAnimeList.filter((myanime)=>{
+      return myanime.mal_id !== anime.mal_id
+    })
+    setMyAnimeList(newArray)
+  }
 
   const getData = async() => {
     const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
@@ -28,27 +47,39 @@ function App() {
   }, [search]) // Pass search, whenever search value is changed useEffect is ran
   return (
     <>
-      <div className='header'>
-        <h1>Anime Empire</h1>
-          <div className='search-box'>
-              <input type="search" placeholder='Find your Anime' 
-              onChange={(e) => setSearch(e.target.value)}/>
-            </div> 
-      </div>
+        <div className="header">
+          <h1>Anime Database</h1>
+          <div className="search-box">
+              <input type="search" placeholder="Search your anime" 
+              onChange={(e)=>setSearch(e.target.value)}/>
+          </div>
+        </div>
 
-      <div className='container'>
-        <div className='anime-info'>
-        {/* if animeInfo contains data, then we display animeInfo component */}
-          {animeInfo && <AnimeInfo animeInfo={animeInfo}/>}  
-          <div className='anime-row'>
-            <h2 className='text-heading'>Anime</h2>
-            <div className='row'>
-              <AnimeList animelist = {animeData} />
-              setAnimeInfo={setAnimeInfo}
+        <div className="container">
+          <div className="animeInfo">
+           {animeInfo && <AnimeInfo animeInfo={animeInfo}/>}
+          </div>
+          <div className="anime-row">
+            <h2 className="text-heading">Anime</h2>
+            <div className="row">
+                <AnimeList 
+                animelist={animeData}
+                setAnimeInfo={setAnimeInfo}
+                animeComponent={AddToList}
+                handleList={(anime)=>addTo(anime)}
+                />
+            </div>
+            <h2 className="text-heading">My List</h2>
+            <div className="row">
+                <AnimeList 
+                animelist={myAnimeList}
+                setAnimeInfo={setAnimeInfo}
+                animeComponent={RemoveFromList}
+                handleList={(anime)=>removeFrom(anime)}
+                />
             </div>
           </div>
         </div>
-      </div>
     </>
   );
 }
--- anime-db/src/Components/AnimeList.js
+++ anime-db/src/Components/AnimeList.js
@@ -1,23 +1,32 @@
 import React from 'react'
 
-export const AnimeList = ({animelist, setAnimeInfo}) => {
+export const AnimeList = ({ animelist,setAnimeInfo,animeComponent,handleList}) => {
+  const AddToList=animeComponent;
   return (
     <>
-        {
-            // If animeList has data, it will display, otherwise not found
-            animelist ? (
-                animelist.map((anime, index) => {
-                    return(
-                        <div className="card" key={index} onClick={()=>setAnimeInfo(anime)}>
-                        <img src={anime.images.jpg.large_image_url} alt="animeImage" />
-                        <div className="anime-info">
-                        <h4>{anime.title}</h4>
-                        </div>
-                     </div>
-                    )
-                })
-            ): "Not Found"
-        }
+      {
+        animelist ? (
+          animelist.map((anime, index) => {
+            return (
+              <div className="card" key={index} onClick={()=>setAnimeInfo(anime)}>
+                <img src={anime.images.jpg.large_image_url} alt="animeImage" />
+                <div className="anime-info">
+                  <h4>{anime.title}</h4>
+                  <div className="overlay" onClick={()=>handleList(anime)}>
+                      <h4>{anime.title_japanese}</h4>
+                      <h3>SYNOPSIS</h3>
+                      <div className="synopsis">
+                        <p>{anime.synopsis}</p>
+                      </div>
+                      <AddToList/>
+                  </div>
+
+                </div>
+              </div>
+            )
+          })
+        ) : "Not Found"
+      }
 
     </>
   )
